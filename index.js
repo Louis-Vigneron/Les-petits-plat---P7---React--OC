687bd8c887b1625627f61a1442cb13a52303a376
Louis.V <loulou38101@gmail.com>
2023-08-09 19:51:38 +0200

    add features for avoid duplicate choice sort

--- index.js
+++ index.js
@@ -11,32 +11,32 @@ function recoveryRecipes() {
 
     totalRecipes.textContent = `${recipes.length} recettes`
 
-    recipes.forEach(el => {
-        main.innerHTML +=
-            `   
-        <div class="main__card">
-            <img class="main__card__img" src="./Assets/Photos Recettes/${el.image}" alt="${el.name}">
-            <div class="main__card__time">${el.time} min</div>
-            <div class="main__card__description">
-                <h2 class="main__card__description__title">${el.name}</h2>
-                <div class="main__card__description__recipe">
-                    <h3 class="main__card__description__recipe__title">RECETTE</h3>
-                    <p class="main__card__description__recipe__text">${el.description}</p>
-                </div>
-                <div class="main__card__description__ingredient">
-                    <h3 class="main__card__description__ingredient__title">INGRÉDIENTS</h3>     
-                    ${el.ingredients.map((ingredient) =>
-                `   <div class="main__card__description__ingredient__tags">
-                                <p class="main__card__description__ingredient__tags__type">${ingredient.ingredient}</p>
-                                <p class="main__card__description__ingredient__tags__quantity">${ingredient.quantity ? ingredient.quantity : '-'} ${ingredient.unit ? ingredient.unit : ''}</p>
-                            </div>
-                            `).join('')
-            }       
-                </div>
-            </div>
-        </div>    
-    `
-    });
+    /*  recipes.forEach(el => {
+         main.innerHTML +=
+             `   
+         <div class="main__card">
+             <img class="main__card__img" src="./Assets/Photos Recettes/${el.image}" alt="${el.name}">
+             <div class="main__card__time">${el.time} min</div>
+             <div class="main__card__description">
+                 <h2 class="main__card__description__title">${el.name}</h2>
+                 <div class="main__card__description__recipe">
+                     <h3 class="main__card__description__recipe__title">RECETTE</h3>
+                     <p class="main__card__description__recipe__text">${el.description}</p>
+                 </div>
+                 <div class="main__card__description__ingredient">
+                     <h3 class="main__card__description__ingredient__title">INGRÉDIENTS</h3>     
+                     ${el.ingredients.map((ingredient) =>
+                 `   <div class="main__card__description__ingredient__tags">
+                                 <p class="main__card__description__ingredient__tags__type">${ingredient.ingredient}</p>
+                                 <p class="main__card__description__ingredient__tags__quantity">${ingredient.quantity ? ingredient.quantity : '-'} ${ingredient.unit ? ingredient.unit : ''}</p>
+                             </div>
+                             `).join('')
+             }       
+                 </div>
+             </div>
+         </div>    
+     `
+     }); */
 
     sortList(ingredients, 'ingredients', 'ingredient');
     sortList(appliance, 'appliance', '');
@@ -64,10 +64,11 @@ function displaySortOptions() {
 
 
 }
-
+let alreadySelect = []
 function selectSortOption() {
     const options = document.querySelectorAll('[role="listbox"]');
     const displayOption = document.getElementById('selectedOption')
+
     options.forEach(el => {
         el.addEventListener("click", () => {
             let listOptions = el.closest("ul");
@@ -78,12 +79,17 @@ function selectSortOption() {
             el.setAttribute('aria-selected', "true");
             let selectedOption = listOptions.querySelector('[aria-selected="true"]').id;
             let displayOptionList = listOptions.previousElementSibling;
-            displayOptionList.innerHTML += `<p class="main__sort__list__select__option" data-custom-value="${selectedOption}">${selectedOption} <i class="fa-solid fa-circle-xmark closeOptionSelect closeOptionSelect--list"></i></i></p>`;
-            displayOption.innerHTML += `<p class="main__selected__option" data-custom-value="${selectedOption}">${selectedOption} <i class="fa-solid fa-xmark closeOptionSelect"></i></p>`;            
-            cleanOptionSelect();
+            if (!alreadySelect.includes(selectedOption)) {
+                displayOptionList.innerHTML += `<p class="main__sort__list__select__option" data-custom-value="${selectedOption}">${selectedOption} <i class="fa-solid fa-circle-xmark closeOptionSelect closeOptionSelect--list"></i></i></p>`;
+                displayOption.innerHTML += `<p class="main__selected__option" data-custom-value="${selectedOption}">${selectedOption} <i class="fa-solid fa-xmark closeOptionSelect"></i></p>`;
+                alreadySelect.push(selectedOption);
+                cleanOptionSelect();
+            }
+
+
         })
     });
-    
+
 }
 
 function sortList(array, types, type) {
@@ -114,14 +120,15 @@ function sortList(array, types, type) {
 }
 
 function cleanOptionSelect() {
-    let button = document.querySelectorAll(".closeOptionSelect");      
+    let button = document.querySelectorAll(".closeOptionSelect");
     button.forEach(el => {
         el.addEventListener("click", () => {
-             let selectRemoveOption = el.closest("p");
-             let removeOption = document.querySelectorAll(`[data-custom-value="${selectRemoveOption.dataset.customValue}"]`);     
-             removeOption.forEach(el=>{
+            let selectRemoveOption = el.closest("p");
+            let removeOption = document.querySelectorAll(`[data-custom-value="${selectRemoveOption.dataset.customValue}"]`);
+            alreadySelect = alreadySelect.filter((e) => e !== selectRemoveOption.dataset.customValue);
+            removeOption.forEach(el => {
                 el.remove()
-             })
+            })
         })
     })
 }
